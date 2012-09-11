e47ddee244e193f7f57b381a4660586d6dc33aba
Alexandra Keller <alexandra@eye3ware.com>
2012-09-11 13:33:43 +0200

    tests for title added

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,9 +4,44 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'eye3ware'" do
+   it "should have the h1 'eye3ware'" do
       visit '/static_pages/home'
-      page.should have_content('eye3ware')
+      page.should have_selector('h1', :text => 'eye3ware')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "eye3ware | Home")
     end
   end
+  
+  describe "Contact page" do
+
+   it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                        :text => "eye3ware | Contact")
+    end
+  end
+  
+ describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                    :text => "eye3ware | About Us")
+    end
+  end
+  
 end
