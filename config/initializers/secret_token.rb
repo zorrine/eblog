da15a81cac86c91c93b2387b50802c6740105202
Alexandra Keller <alexandra@eye3ware.com>
2012-09-11 11:52:31 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a99b23517af4482011b887106243690e0721052e118e269a5dcd0064e663eda96f8bc46d79f66a83191b5efd589995df70b108b00b295584b781e2eb9d45d968'
